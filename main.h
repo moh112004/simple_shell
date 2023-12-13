fc516fbc7be36be2a3e6669ca343fdace6b3357e
moh112004 <almstfym870@gmail.com>
2023-12-13 21:08:08 +0300

    upload files

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 int executeCommand(char *command);
+
+#endif
